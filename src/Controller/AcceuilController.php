c1cedd22871276bbd24915a3a07c4b93f9a39153
Tom Fabre <tom.fabre34630@gmail.com>
2026-01-13 17:42:05 +0100

    Phase 2

--- src/Controller/AcceuilController.php
+++ src/Controller/AcceuilController.php
@@ -7,14 +7,15 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class AcceuilController {
+class AcceuilController extends AbstractController{
     
     #[Route('/', name: 'acceuil')]
     public function index(): Response {
-        return new Response('Hello World');
+        return $this->render ("pages/acceuil.html.twig");
     }
 }

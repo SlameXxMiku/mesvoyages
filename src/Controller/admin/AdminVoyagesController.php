0845ded4f38f641d4ca51f4b399cf612250828c5
Tom Fabre <tom.fabre34630@gmail.com>
2026-01-16 03:48:57 +0100

    Phase 11 ORM suppression d'un enregistrement

---
+++ src/Controller/admin/AdminVoyagesController.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Controller\admin;
+
+/**
+ * Description of AdminVoyagesController
+ *
+ * @author SlameX
+ */
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\VisiteRepository;
+use Symfony\Component\HttpFoundation\Request;
+
+class AdminVoyagesController extends AbstractController {
+    
+    #[Route('/admin', name: 'admin.voyages')]
+    public function index(VisiteRepository $repository): Response
+    {
+        $visites = $repository->findAllOrderBy('datecreation','DESC');
+
+        return $this->render('admin/admin.voyages.html.twig', [
+            'visites' => $visites,
+        ]);
+    }
+    #[Route('/voyages/tri/{champ}/{ordre}', name: 'voyages.sort')]
+    public function sort($champ, $ordre,VisiteRepository $repository):Response {
+        
+        $visites=$repository->findAllOrderBy($champ, $ordre);
+        return $this->render("pages/voyages.html.twig",[
+        'visites'=>$visites
+        ]);   
+    }
+    #[Route('/voyages/recherche/{champ}', name: 'voyages.findallequal')]
+    public function findAllEqual ($champ, Request $request ,VisiteRepository $repository):Response{
+        
+        $valeur = $request->get("recherche");
+        $visites = $repository->findByEqualValue($champ, $valeur);
+        return $this->render("pages/voyages.html.twig",[
+            'visites'=> $visites
+        ]);
+    }
+    #[Route('voyages/voyage/{id}',name:'voyages.showone')]
+    public function showOne($id, VisiteRepository $repository) :Response{
+        
+        $visite = $repository->find($id);
+        return $this->render("pages/voyage.html.twig",[
+                'visite'=>$visite
+                ]);
+    }
+    
+    #[Route('/admin/suppr/{id}', name:'admin.voyage.suppr')]
+    public function suppr( $id, VisiteRepository $repository) : Response {
+        $visite = $repository->find($id);
+        $repository->remove($visite);
+        return $this->redirectToRoute('admin.voyages');
+        
+    }
+}
--- src/Repository/VisiteRepository.php
+++ src/Repository/VisiteRepository.php
@@ -42,4 +42,9 @@ class VisiteRepository extends ServiceEntityRepository
                     ->getResult();
         }
     }  
+    public function remove(Visite $visite):void 
+    {
+        $this->getEntityManager()->remove($visite);
+        $this->getEntityManager()->flush();
+    }
 }
